da95e0eb2f645c1bfd73e5165412b0f6b83e29bd
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-11-07 22:51:38 +1100

    CI CD - HealthController added

---
+++ backend/TaskService/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok(new
+        {
+            status = "Healthy",
+            timestamp = DateTime.UtcNow
+        });
+    }
+}
